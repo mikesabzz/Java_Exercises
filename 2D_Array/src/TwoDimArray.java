59046fd9428d4d73487b98cc697a3e0c50b55587
mikesabzz <michaelsabzevari@gmail.com>
2023-06-13 16:01:55 -0400

    Solve a Java problem using 2D array

---
+++ 2D_Array/src/TwoDimArray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class TwoDimArray {
+    public static void main(String[] args) throws Exception {
+    
+    }
+
+    public void Solution(int[][] pot){
+
+    }
+
+}
