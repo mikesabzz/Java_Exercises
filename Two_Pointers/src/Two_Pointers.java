4a78ecf29dbe4a2b3afe8eaffdc94a77f569520d
mikesabzz <michaelsabzevari@gmail.com>
2023-07-12 12:45:28 -0400

    twoSum problem solved

--- Two_Pointers/src/Two_Pointers.java
+++ Two_Pointers/src/Two_Pointers.java
@@ -15,9 +15,28 @@ public class Two_Pointers {
         }
         return true;
     }
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length -1;
+        while(left < right){
+            if(numbers[left] + numbers[right] > target){
+                right--;
+            } else if(numbers[left] + numbers[right] < target){
+                left++;
+            } else {
+                return new int[] {left+1, right+1};
+            }
+        }
+        return new int[] {left, right};
+    }
     public static void main(String[] args) throws Exception {
         Two_Pointers twoP = new Two_Pointers();
         boolean ret = twoP.isPalindrome("ab2a");
+        int[] arr = {2,7,11,15};
+        int[] indices = twoP.twoSum(arr, 9);
+        for(int ind : indices){
+            System.out.println(ind);
+        }
         
     }
 }
