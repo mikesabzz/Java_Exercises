4fe6c284295ccb7d5013c2bd65e163e1e235f20e
mikesabzz <michaelsabzevari@gmail.com>
2023-07-14 13:23:01 -0400

    Three sum problem solved

--- Two_Pointers/src/Two_Pointers.java
+++ Two_Pointers/src/Two_Pointers.java
@@ -29,6 +29,30 @@ public class Two_Pointers {
         }
         return new int[] {left, right};
     }
+    public void threeSum(int[] nums){
+        Arrays.sort(nums);
+        Set<List<Integer>> array_list = new HashSet<List<Integer>>();
+        for(int i=0; i<nums.length; i++){
+            int left = i+1;
+            int right = nums.length-1;
+            while(left < right){
+                if(nums[i] + nums[left] + nums[right] == 0){
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    array_list.add(list);
+                    left++;
+                    right--;
+                } else if (nums[i] + nums[left] + nums[right] > 0){
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        System.out.println(new ArrayList<>(array_list));
+    }
     public static void main(String[] args) throws Exception {
         Two_Pointers twoP = new Two_Pointers();
         boolean ret = twoP.isPalindrome("ab2a");
@@ -37,6 +61,7 @@ public class Two_Pointers {
         for(int ind : indices){
             System.out.println(ind);
         }
-        
+        int[] triplets = {-1,0,1,2,-1,4};
+        twoP.threeSum(triplets);
     }
 }
