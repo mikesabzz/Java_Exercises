6e227bef0a286a954dce2d88e11705d2d74828e9
mikesabzz <michaelsabzevari@gmail.com>
2023-09-11 10:32:44 -0400

    removed maps

--- Hashing/src/Hashing.java
+++ Hashing/src/Hashing.java
@@ -47,14 +47,7 @@ public class Hashing {
     }
     public List<String> letterCombinations(String digits) {
         HashMap<Character, String> map = new HashMap<>();
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
+    
         for(int i=0; i<digits.length(); i++){
             char[] s = map.get(digits.charAt(i)).toCharArray();
             for(int j=0; j<s.length; j++){
